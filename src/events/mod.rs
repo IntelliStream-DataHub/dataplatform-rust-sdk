7c960b7ac2a888fc9080c9038ef76e33fa997726
olav <olav.gjerde@akerbp.com>
2025-05-16 13:35:37 +0200

    working create and delete events

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -4,9 +4,8 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use crate::ApiService;
-use crate::generic::{ApiServiceProvider, DataWrapper, IdAndExtIdCollection, RelationForm};
+use crate::generic::{ApiServiceProvider, DataWrapper, IdAndExtIdCollection};
 use crate::http::ResponseError;
-use crate::timeseries::{TimeSeries};
 
 pub struct EventsService<'a>{
     pub(crate) api_service: Weak<ApiService<'a>>,
@@ -20,21 +19,33 @@ impl<'a> EventsService<'a>{
         EventsService {api_service, base_url}
     }
 
-    pub fn create_events(&self, data: &DataWrapper<TimeSeries>)
-        -> Result<(), ResponseError> {
+    pub async fn create(&self, data: &DataWrapper<Event>)
+        -> Result<DataWrapper<Event>, ResponseError>
+    {
+        let path = &format!("{}/create", self.base_url);
+        self.execute_post_request::<DataWrapper<Event>, _>(path, data).await
+    }
+
+    pub async fn create_one(&self, event: &Event)
+                            -> Result<DataWrapper<Event>, ResponseError>
+    {
+        let mut dw = DataWrapper::new();
+        dw.add_item(event.clone());
+        self.create(&dw).await
+    }
+
+    pub async fn delete(&self, json: &IdAndExtIdCollection)
+                        -> Result<DataWrapper<Event>, ResponseError>
+    {
+        let path = &format!("{}/delete", self.base_url);
+        self.execute_post_request(path, json).await
+    }
+
+    pub fn filter(&self) -> Result<(), ResponseError> {
         unimplemented!()
     }
 
-    pub fn delete_events(&self, data: &DataWrapper<IdAndExtIdCollection>)
-        -> Result<(), ResponseError> {
-        unimplemented!()
-    }
-
-    pub fn filters_events(&self) -> Result<(), ResponseError> {
-        unimplemented!()
-    }
-
-    pub fn list_events(&self) -> Result<(), ResponseError> {
+    pub fn list(&self) -> Result<(), ResponseError> {
         unimplemented!()
     }
 
@@ -43,49 +54,197 @@ impl<'a> EventsService<'a>{
         self.execute_get_request(&self.base_url).await
     }
 
-    pub fn retrieve_events(&self) -> Result<(), ResponseError> {
+    pub fn retrieve(&self) -> Result<(), ResponseError> {
         unimplemented!()
     }
 
-    pub fn search_events(&self) -> Result<(), ResponseError> {
+    pub fn search(&self) -> Result<(), ResponseError> {
         unimplemented!()
     }
 
-    pub fn update_events(&self) -> Result<(), ResponseError> {
+    pub fn update(&self) -> Result<(), ResponseError> {
         unimplemented!()
     }
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Event {
-    pub id: Uuid,
+    pub id: Option<Uuid>,
     #[serde(rename = "externalId")]
     pub external_id: String,
-    pub name: String,
     pub metadata: Option<HashMap<String, String>>,
     pub description: Option<String>,
     #[serde(rename = "type")]
     pub r#type: Option<String>,
+    #[serde(rename = "subType")]
     pub sub_type: Option<String>,
     #[serde(rename = "dataSetId")]
     pub data_set_id: Option<u64>,
-    #[serde(rename = "valueType")]
-    pub value_type: String,
-    #[serde(rename = "createdTime")]
-    pub created_time: Option<u64>,
-    #[serde(rename = "lastUpdatedTime")]
-    pub last_updated_time: Option<u64>,
+    #[serde(skip_serializing)]
+    #[serde(rename = "createdTimeHR")]
+    pub created_time: Option<DateTime<Utc>>,
+    // Read from "createdTimeHR" when deserializing, but emit "createdTime" on serialization
+    #[serde(skip_serializing)]
+    #[serde(rename = "lastUpdatedTimeHR")]
+    pub last_updated_time: Option<DateTime<Utc>>,
     #[serde(rename = "relatedResourceIds")]
     pub related_resource_ids: Vec<u64>,
     #[serde(rename = "relatedResourceExternalIds")]
     pub related_resource_external_ids: Vec<String>,
     pub source: Option<String>,
-    #[serde(rename = "startTime")]
+    #[serde(rename(serialize = "startTime", deserialize = "startTimeHR"))]
     pub start_time: Option<DateTime<Utc>>,
-    #[serde(rename = "endTime")]
+    #[serde(rename(serialize = "endTime", deserialize = "endTimeHR"))]
     pub end_time: Option<DateTime<Utc>>,
 }
 
 impl Event {
 
+    pub fn new(external_id: String) -> Self {
+        Event{
+            id: None,
+            external_id,
+            metadata: None,
+            description: None,
+            r#type: None,
+            sub_type: None,
+            data_set_id: None,
+            created_time: None,
+            last_updated_time: None,
+            related_resource_ids: vec![],
+            related_resource_external_ids: vec![],
+            source: None,
+            start_time: None,
+            end_time: None,
+        }
+    }
+
+    pub fn add_metadata(&mut self, key: String, value: String) {
+        if self.metadata.is_none() {
+            self.metadata = Some(HashMap::new());
+        }
+        self.metadata.as_mut().unwrap().insert(key, value);
+    }
+
+    pub fn remove_metadata(&mut self, key: String) {
+        if self.metadata.is_some() {
+            self.metadata.as_mut().unwrap().remove(&key);
+        }
+    }
+
+    pub fn add_related_resource_id(&mut self, id: u64) {
+        self.related_resource_ids.push(id);
+    }
+
+    pub fn remove_related_resource_id(&mut self, id: u64) {
+        self.related_resource_ids.retain(|&x| x != id);
+    }
+
+    pub fn add_related_resource_external_id(&mut self, external_id: String) {
+        self.related_resource_external_ids.push(external_id);
+    }
+
+    pub fn remove_related_resource_external_id(&mut self, external_id: String) {
+        self.related_resource_external_ids.retain(|x| x != &external_id);
+    }
+
+    pub fn get_id(&self) -> Option<&Uuid> {
+        self.id.as_ref()
+    }
+
+    pub fn get_external_id(&self) -> &str {
+        self.external_id.as_str()
+    }
+
+    pub fn set_external_id(&mut self, external_id: String) {
+        self.external_id = external_id;
+    }
+
+    pub fn get_metadata(&self) -> Option<&HashMap<String, String>> {
+        self.metadata.as_ref()
+    }
+
+    pub fn get_type(&self) -> Option<&str> {
+        self.r#type.as_deref()
+    }
+
+    pub fn set_type(&mut self, r#type: String) {
+        self.r#type = Some(r#type);
+    }
+
+    pub fn get_sub_type(&self) -> Option<&str> {
+        self.sub_type.as_deref()
+    }
+
+    pub fn set_sub_type(&mut self, sub_type: String) {
+        self.sub_type = Some(sub_type);
+    }
+
+    pub fn get_data_set_id(&self) -> Option<u64> {
+        self.data_set_id
+    }
+
+    pub fn get_data_set_id_as_ref(&self) -> Option<&u64> {
+        self.data_set_id.as_ref()
+    }
+
+    pub fn set_data_set_id(&mut self, data_set_id: u64) {
+        self.data_set_id = Some(data_set_id);
+    }
+
+    pub fn get_description(&self) -> Option<&str> {
+        self.description.as_deref()
+    }
+
+    pub fn set_description(&mut self, description: String) {
+        self.description = Some(description);
+    }
+
+    pub fn get_source(&self) -> Option<&str> {
+        self.source.as_deref()
+    }
+
+    pub fn set_source(&mut self, source: String) {
+        self.source = Some(source);
+    }
+
+    pub fn get_start_time(&self) -> Option<&DateTime<Utc>> {
+        self.start_time.as_ref()
+    }
+
+    pub fn set_start_time(&mut self, start_time: DateTime<Utc>) {
+        self.start_time = Some(start_time);
+    }
+
+    pub fn get_end_time(&self) -> Option<&DateTime<Utc>> {
+        self.end_time.as_ref()
+    }
+
+    pub fn set_end_time(&mut self, end_time: DateTime<Utc>) {
+        self.end_time = Some(end_time);
+    }
+
+    pub fn get_related_resource_ids(&self) -> &Vec<u64> {
+        &self.related_resource_ids
+    }
+
+    pub fn get_related_resource_external_ids(&self) -> &Vec<String> {
+        &self.related_resource_external_ids
+    }
+
+    pub fn get_metadata_keys(&self) -> Option<Vec<&str>> {
+        self.metadata.as_ref().map(|m| m.keys().map(|k| k.as_str()).collect())
+    }
+
+    pub fn get_metadata_value(&self, key: &str) -> Option<&str> {
+        self.metadata.as_ref().and_then(|m| m.get(key)).map(|v| v.as_str())
+    }
+
+    pub fn get_created_time(&self) -> Option<&DateTime<Utc>> {
+        self.created_time.as_ref()
+    }
+
+    pub fn get_last_updated_time(&self) -> Option<&DateTime<Utc>> {
+        self.last_updated_time.as_ref()
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -56,14 +56,17 @@ fn create_api_service() -> Rc<ApiService<'static>> {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
     use std::fs::File;
     use std::io::Read;
+    use std::thread::sleep;
     use super::*;
     use crate::timeseries::{TimeSeries, TimeSeriesUpdate, TimeSeriesUpdateFields};
     use maplit::hashmap;
     use reqwest::StatusCode;
-    use crate::generic::{DataWrapper, Datapoint, DatapointString, DatapointsCollection, DeleteFilter, IdAndExtId, RetrieveFilter};
+    use crate::generic::{DataWrapper, DatapointString, DatapointsCollection, DeleteFilter, IdAndExtId, RetrieveFilter};
     use chrono::{DateTime, Duration, TimeZone, Utc};
+    use crate::events::Event;
     use crate::http::ResponseError;
 
     #[tokio::test]
@@ -271,7 +274,6 @@ mod tests {
         match result {
             Ok(timeseries) => {
                 assert_eq!(timeseries.length(), 0);
-
             },
             Err(e) => {
                 println!("{:?}", e.get_message());
@@ -1098,9 +1100,78 @@ mod tests {
 
     #[tokio::test]
     async fn test_create_and_delete_events() -> Result<(), Box<dyn std::error::Error>> {
+
+        let api_service = create_api_service();
         
         let unique_id: u64 = 7010;
+        let event_time: DateTime<Utc> = Utc.with_ymd_and_hms(2025, 5, 17, 0, 0, 0).unwrap();
+        let external_id = format!("valve_event_alarm_{:?}", unique_id);
+
+        delete_events(&api_service,vec![&external_id]).await;
         
+        let mut new_event = Event::new(
+            external_id.clone(),
+        );
+        new_event.metadata = Option::from(
+            HashMap::from([
+                ("hostname".to_string(), "sveinar".to_string()),
+                ("time".to_string(), "2025-05-16T09:04:53.257Z".to_string()),
+            ])
+        );
+        new_event.start_time = Option::from(event_time);
+        new_event.r#type = Option::from("valve".to_string());
+        new_event.sub_type = Option::from("alarm".to_string());
+        new_event.description = Option::from("Gas valve attached to pipe AS-PIP-2452".to_string());
+        //new_event.related_resource_external_ids = vec!["AS-PIP-2452".to_string()];
+        //new_event.related_resource_ids = vec![1234];
+
+        new_event.add_metadata("version".to_string(), "0x0".to_string());
+        new_event.add_metadata("TEST-REMOVE".to_string(), "foobar".to_string());
+        new_event.remove_metadata("TEST-REMOVE".to_string());
+        new_event.set_source("valve-events".to_string());
+        // TODO: uncomment when resources api is working
+        /*new_event.add_related_resource_id(2345);
+        new_event.add_related_resource_id(1111);
+        new_event.remove_related_resource_id(1111);
+        new_event.add_related_resource_external_id("AS-PLP-2333".to_string());
+        new_event.add_related_resource_external_id("TEST-REMOVE".to_string());
+        new_event.remove_related_resource_external_id("TEST-REMOVE".to_string());*/
+
+        let result = api_service.events.create_one(&new_event).await;
+        match result {
+            Ok(events) => {
+                assert_eq!(events.length(), 1);
+                let event = events.get_items().first().unwrap();
+                assert_eq!((event.get_id()).is_some(), true);
+                assert_eq!(event.get_external_id(), &external_id);
+                assert_eq!(event.get_type().unwrap(), "valve");
+                assert_eq!(event.get_sub_type().unwrap(), "alarm");
+                assert_eq!(event.get_source().unwrap(), "valve-events");
+                println!("Event created successfully!");
+            },
+            Err(e) => {
+                eprintln!("{:?}", e.get_message());
+                assert_eq!(e.status.as_u16(), 201);
+                panic!("error with Event create");
+            }
+        }
+        
+
         Ok(())
+    }
+    
+    async fn delete_events(api_service: &ApiService<'_>, events: Vec<&str>) {
+        let id_collection = IdAndExtIdCollection::from_external_id_vec(events);
+        let delete_result = api_service.events.delete(&id_collection).await;
+        match delete_result {
+            Ok(events) => {
+                assert_eq!(events.length(), 0);
+            },
+            Err(e) => {
+                eprintln!("{:?}", e.get_message());
+                assert_eq!(e.status.as_u16(), 200);
+            }
+        }
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     }
 }
