f11e167ab21084fc12b268555d317a608eedfcdc
Olav Gjerde <olav@intellistream.ai>
2026-04-20 13:40:58 +0200

    updated test to use float value type and simplified datapoint value assignment

--- src/timeseries/test.rs
+++ src/timeseries/test.rs
@@ -384,7 +384,7 @@ mod tests {
                     "foodda".to_string() => "bardda".to_string(),
                     "bardda".to_string() => "bazdda".to_string()
                 })
-            .set_value_type("bigint").clone();
+            .set_value_type("float").clone();
 
         ts_collection.add_item(ts1);
         ts_collection.add_item(ts2);
@@ -430,7 +430,7 @@ mod tests {
         let datetime = Utc.with_ymd_and_hms(2025, 2, 4, 9, 0, 0).unwrap();
         dp_collection.datapoints = create_daily_datapoints(datetime);
         for dp in &mut dp_collection.datapoints {
-            dp.value = (dp.value.parse::<f64>().unwrap() as i64).to_string();
+            dp.value = dp.value.clone();
         }
 
         data_request.get_items_mut().push(dp_collection);
