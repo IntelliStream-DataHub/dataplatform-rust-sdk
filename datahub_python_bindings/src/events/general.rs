cefc6007dda2ac9e0fa026a7993118d7a604ea10
jgjesdal <jostein@intellistream.ai>
2026-06-12 12:43:04 +0200

    add setter methods for units,timeseries,resources, events and datasets in python sdk.

--- datahub_python_bindings/src/events/general.rs
+++ datahub_python_bindings/src/events/general.rs
@@ -57,25 +57,49 @@ impl PyEvent {
     pub fn external_id(&self) -> &str {
         self.inner.get_external_id()
     }
+    #[setter]
+    pub fn set_external_id(&mut self, value: String) {
+        self.inner.external_id = value;
+    }
     #[getter]
     pub fn metadata(&self) -> Option<&HashMap<String, String>> {
         self.inner.get_metadata()
+    }
+    #[setter]
+    pub fn set_metadata(&mut self, value: Option<HashMap<String, String>>) {
+        self.inner.metadata = value;
     }
     #[getter]
     pub fn description(&self) -> Option<&str> {
         self.inner.get_description()
     }
+    #[setter]
+    pub fn set_description(&mut self, value: Option<String>) {
+        self.inner.description = value;
+    }
     #[getter]
     pub fn r#type(&self) -> Option<&str> {
         self.inner.get_type()
+    }
+    #[setter]
+    pub fn set_type(&mut self, value: Option<String>) {
+        self.inner.r#type = value;
     }
     #[getter]
     pub fn sub_type(&self) -> Option<&str> {
         self.inner.get_sub_type()
     }
+    #[setter]
+    pub fn set_sub_type(&mut self, value: Option<String>) {
+        self.inner.sub_type = value;
+    }
     #[getter]
     pub fn status(&self) -> Option<&str> {
         self.inner.get_status()
+    }
+    #[setter]
+    pub fn set_status(&mut self, value: Option<String>) {
+        self.inner.status = value;
     }
     #[getter]
     pub fn created_time(&self) -> Option<&DateTime<Utc>> {
@@ -89,20 +113,40 @@ impl PyEvent {
     pub fn data_set_id(&self) -> Option<u64> {
         self.inner.get_data_set_id()
     }
+    #[setter]
+    pub fn set_data_set_id(&mut self, value: Option<u64>) {
+        self.inner.data_set_id = value;
+    }
     #[getter]
     pub fn related_resource_ids(&self) -> &Vec<u64> {
         self.inner.get_related_resource_ids()
+    }
+    #[setter]
+    pub fn set_related_resource_ids(&mut self, value: Vec<u64>) {
+        self.inner.related_resource_ids = value;
     }
     #[getter]
     pub fn related_resource_external_ids(&self) -> &Vec<String> {
         self.inner.get_related_resource_external_ids()
     }
+    #[setter]
+    pub fn set_related_resource_external_ids(&mut self, value: Vec<String>) {
+        self.inner.related_resource_external_ids = value;
+    }
     #[getter]
     pub fn source(&self) -> Option<&str> {
         self.inner.get_source()
     }
+    #[setter]
+    pub fn set_source(&mut self, value: Option<String>) {
+        self.inner.source = value;
+    }
     #[getter]
     pub fn event_time(&self) -> Option<&DateTime<Utc>> {
         self.inner.get_event_time()
+    }
+    #[setter]
+    pub fn set_event_time(&mut self, value: Option<DateTime<Utc>>) {
+        self.inner.event_time = value;
     }
 }
