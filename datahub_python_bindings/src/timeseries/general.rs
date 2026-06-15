0d07fd5414ad8ff18bcc5029bd41c8d263dce564
JosteinGj <66584533+JosteinGj@users.noreply.github.com>
2026-06-15 12:48:10 +0200

    add setter methods for units,timeseries,resources, events and datasets in python sdk. (#17)
    Co-authored-by: jgjesdal <jostein@intellistream.ai>

--- datahub_python_bindings/src/timeseries/general.rs
+++ datahub_python_bindings/src/timeseries/general.rs
@@ -1,4 +1,4 @@
-use super::PyTimeSeries;
+use super::{PyTimeSeries, ValueType};
 use chrono::{DateTime, Utc};
 use pyo3::pymethods;
 use std::collections::HashMap;
@@ -13,41 +13,77 @@ impl PyTimeSeries {
     pub fn external_id(&self) -> &str {
         &self.inner.external_id.as_str()
     }
+    #[setter]
+    pub fn set_external_id(&mut self, value: String) {
+        self.inner.external_id = value;
+    }
 
     #[getter]
     pub fn name(&self) -> &String {
         &self.inner.name
+    }
+    #[setter]
+    pub fn set_name(&mut self, value: String) {
+        self.inner.name = value;
     }
 
     #[getter]
     pub fn metadata(&self) -> Option<&HashMap<String, String>> {
         self.inner.metadata.as_ref()
     }
+    #[setter]
+    pub fn set_metadata(&mut self, value: Option<HashMap<String, String>>) {
+        self.inner.metadata = value;
+    }
 
     #[getter]
     pub fn unit(&self) -> Option<&str> {
         self.inner.unit.as_deref()
+    }
+    #[setter]
+    pub fn set_unit(&mut self, value: Option<String>) {
+        self.inner.unit = value;
     }
 
     #[getter]
     pub fn description(&self) -> Option<&str> {
         self.inner.description.as_deref()
     }
+    #[setter]
+    pub fn set_description(&mut self, value: Option<String>) {
+        self.inner.description = value;
+    }
     #[getter]
     pub fn unit_external_id(&self) -> Option<&str> {
         self.inner.unit_external_id.as_deref()
+    }
+    #[setter]
+    pub fn set_unit_external_id(&mut self, value: Option<String>) {
+        self.inner.unit_external_id = value;
     }
     #[getter]
     pub fn security_categories(&self) -> Option<&Vec<u64>> {
         self.inner.security_categories.as_ref()
     }
+    #[setter]
+    pub fn set_security_categories(&mut self, value: Option<Vec<u64>>) {
+        self.inner.security_categories = value;
+    }
     #[getter]
     pub fn data_set_id(&self) -> Option<u64> {
         self.inner.data_set_id
     }
+    #[setter]
+    pub fn set_data_set_id(&mut self, value: Option<u64>) {
+        self.inner.data_set_id = value;
+    }
     #[getter]
     pub fn value_type(&self) -> &str {
         &self.inner.value_type.as_str()
+    }
+    #[setter]
+    pub fn set_value_type(&mut self, value: ValueType) {
+        self.inner.value_type = value.to_string();
     }
     #[getter]
     pub fn created_time(&self) -> Option<DateTime<Utc>> {
--- datahub_python_bindings/src/units/general.rs
+++ datahub_python_bindings/src/units/general.rs
@@ -5,19 +5,98 @@ impl PyUnit {
     fn id(&self) -> u64 {
         self.inner.id
     }
+    #[setter]
+    fn set_id(&mut self, value: u64) {
+        self.inner.id = value;
+    }
 
     #[getter]
     fn external_id(&self) -> String {
         self.inner.external_id.clone()
+    }
+    #[setter]
+    fn set_external_id(&mut self, value: String) {
+        self.inner.external_id = value;
     }
 
     #[getter]
     fn name(&self) -> String {
         self.inner.name.clone()
     }
+    #[setter]
+    fn set_name(&mut self, value: String) {
+        self.inner.name = value;
+    }
+
+    #[getter]
+    fn long_name(&self) -> String {
+        self.inner.long_name.clone()
+    }
+    #[setter]
+    fn set_long_name(&mut self, value: String) {
+        self.inner.long_name = value;
+    }
 
     #[getter]
     fn symbol(&self) -> String {
         self.inner.symbol.clone()
+    }
+    #[setter]
+    fn set_symbol(&mut self, value: String) {
+        self.inner.symbol = value;
+    }
+
+    #[getter]
+    fn description(&self) -> String {
+        self.inner.description.clone()
+    }
+    #[setter]
+    fn set_description(&mut self, value: String) {
+        self.inner.description = value;
+    }
+
+    #[getter]
+    fn alias_names(&self) -> Vec<String> {
+        self.inner.alias_names.clone()
+    }
+    #[setter]
+    fn set_alias_names(&mut self, value: Vec<String>) {
+        self.inner.alias_names = value;
+    }
+
+    #[getter]
+    fn quantity(&self) -> String {
+        self.inner.quantity.clone()
+    }
+    #[setter]
+    fn set_quantity(&mut self, value: String) {
+        self.inner.quantity = value;
+    }
+
+    #[getter]
+    fn conversion(&self) -> HashMap<String, f64> {
+        self.inner.conversion.clone()
+    }
+    #[setter]
+    fn set_conversion(&mut self, value: HashMap<String, f64>) {
+        self.inner.conversion = value;
+    }
+
+    #[getter]
+    fn source(&self) -> String {
+        self.inner.source.clone()
+    }
+    #[setter]
+    fn set_source(&mut self, value: String) {
+        self.inner.source = value;
+    }
+
+    #[getter]
+    fn source_reference(&self) -> String {
+        self.inner.source_reference.clone()
+    }
+    #[setter]
+    fn set_source_reference(&mut self, value: String) {
+        self.inner.source_reference = value;
     }
 }
